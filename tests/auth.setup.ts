6f9e39832d5ed9ecafb243f4ec9d61396ee13e54
Michael-Kruggel <michael.kruggel@defenseunicorns.com>
2024-07-18 19:12:50 +0000

    CI debug

--- tests/auth.setup.ts
+++ tests/auth.setup.ts
@@ -2,24 +2,27 @@ import { test as setup, expect } from '@playwright/test';
 import { authFile } from './playwright.config';
 
 setup('authenticate', async ({ page, context, baseURL }) => {
-  await page.goto(baseURL);
-  await page.getByLabel("Username or email").fill("doug");
-  await page.getByLabel("Password").fill("unicorn123!@#");
-  await page.getByRole("button", { name: "Log In" }).click();
+  try {
+    await page.goto(baseURL);
+    await page.getByLabel("Username or email").fill("doug");
+    await page.getByLabel("Password").fill("unicorn123!@#");
+    await page.getByRole("button", { name: "Log In" }).click();
 
-  // ensure auth cookies were set
-  const cookies = await context.cookies();
-  const keycloakCookie = cookies.find(
-    (cookie) => cookie.name === "KEYCLOAK_SESSION",
-  );
+    // ensure auth cookies were set
+    const cookies = await context.cookies();
+    const keycloakCookie = cookies.find(
+      (cookie) => cookie.name === "KEYCLOAK_SESSION",
+    );
 
-  expect(keycloakCookie).toBeDefined();
-  expect(keycloakCookie?.value).not.toBe("");
-  expect(keycloakCookie?.domain).toContain("sso.");
+    expect(keycloakCookie).toBeDefined();
+    expect(keycloakCookie?.value).not.toBe("");
+    expect(keycloakCookie?.domain).toContain("sso.");
 
-  await page.context().storageState({ path: authFile });
+    await page.context().storageState({ path: authFile });
 
-  await page.waitForURL(baseURL);  // successful redirect
-
-  await expect(page).toHaveURL(baseURL);
+    await expect(page).toHaveURL(baseURL);
+  } catch (error) {
+    await page.screenshot({ path: 'screenshot.png' });
+    throw error;  // rethrow the error after taking the screenshot
+}
 })
